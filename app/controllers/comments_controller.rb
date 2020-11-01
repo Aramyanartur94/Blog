83b620e526320a4d9421cf3dc3baf1763b2e234d
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2020-11-01 15:50:45 +0900

    Show comments on article page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,8 +5,8 @@ class CommentsController < ApplicationController
     @article.comments.create(comment_params)
 
     redirect_to article_path(@article)
-  end
-   
+  end 
+    
     private
 
    def comment_params
