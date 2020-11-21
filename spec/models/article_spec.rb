c180452debafbf6a1ea6b9330ca31aa75d55717f
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2020-11-21 20:53:01 +0900

    Update article spec

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -22,4 +22,15 @@ describe Article do
 
     end
   end
+
+  describe "#last_comment" do
+    it "return the last comment"do
+     #создает статью, но в этот раз с комментариями
+     article = create(:article_with_comments)
+
+     # проверка
+     expect(article.last_comment.body).to eq "Comment body 3"
+    end
+  end
+
 end
