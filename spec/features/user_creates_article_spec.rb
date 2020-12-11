dcdac3113fd743f132e97450b5425eb3c3846b70
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2020-12-11 21:53:15 +0900

    And database_cleaner

---
+++ spec/features/user_creates_article_spec.rb
@@ -0,0 +1,12 @@
+require "rails_helper"
+require "database_cleaner"
+feature "Article Creation" do
+  before(:each) do
+    sign_up
+  end
+
+  scenario "allows user to visit new article page" do
+    visit new_article_path
+    expect(page).to have_content I18n.t('articles.new_article')
+end
+end
--- spec/session_helper.rb
+++ spec/session_helper.rb
@@ -1,8 +1,8 @@
 def sign_up
   visit new_user_registration_path
   
-  fill_in :user_email, :with => 'user@example.com' 
-  fill_in :user_username, :with => 'mike' 
+  fill_in :user_email, :with => 'user@examplee.comm' 
+  fill_in :user_username, :with => 'mikess' 
   fill_in :user_password, :with => 'secure123!#@' 
   fill_in :user_password_confirmation, :with => 'secure123!#@' 
 
