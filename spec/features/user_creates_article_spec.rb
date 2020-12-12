ad761a851949a10485b7052d8d0d81ccab32538f
Aramyanartur94 <anastasiasemilova2012@yandex.ru>
2020-12-12 20:25:23 +0900

    Add user_creates_article test

--- spec/features/user_creates_article_spec.rb
+++ spec/features/user_creates_article_spec.rb
@@ -9,4 +9,15 @@ feature "Article Creation" do
     visit new_article_path
     expect(page).to have_content I18n.t('articles.new_article')
 end
+
+  scenario "allows a user to create new article" do
+    visit new_article_path
+
+    fill_in :article_title, :with => 'good new'
+    fill_in :article_text, :with => 'hi, this is good new'
+
+    click_button 'Save Article'
+
+    expect(page).to have_content 'good new'
+  end
 end
